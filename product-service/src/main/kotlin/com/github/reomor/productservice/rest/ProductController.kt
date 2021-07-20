db9099efec2f8a6b07dac97cd4cabaa0080ee5fa
Reomor <reomor@gmail.com>
2021-07-21 00:23:15 +0300

    api gateway + inner load balancing

--- product-service/src/main/kotlin/com/github/reomor/productservice/rest/ProductController.kt
+++ product-service/src/main/kotlin/com/github/reomor/productservice/rest/ProductController.kt
@@ -1,15 +1,18 @@
 package com.github.reomor.productservice.rest
 
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("$V1$PRODUCTS")
-class ProductController {
+class ProductController(
+    private val environment: Environment
+) {
 
     @GetMapping
     fun get(): String {
-        return "GET"
+        return "GET" + environment.getProperty("local.server.port")
     }
 }
